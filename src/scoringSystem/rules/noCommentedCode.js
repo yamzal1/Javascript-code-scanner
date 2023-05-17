ea3931383ae5b3207060d3fd600a98caba1fba0a
leo.nguyentuong <leo.nguyentuong@gmail.com>
2023-05-17 16:58:05 +0200

    noCommentedCode updated

--- src/scoringSystem/rules/noCommentedCode.js
+++ src/scoringSystem/rules/noCommentedCode.js
@@ -1,8 +1,9 @@
 module.exports = function noCommentedCode(comment, parser){
+    if(comment.split(' ').length === 1) return true
     try{
         parser.parse(comment)
         return false
-    } catch (err) { // the comment is not commented code
+    } catch (err) { // if there is an exception the comment is not commented code
         return true;
     }
 }
