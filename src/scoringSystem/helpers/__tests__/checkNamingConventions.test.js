10a6115c8aa0af23f5281ff4ab3296de96b62cbe
“LAMIASASAOUD” <sasaoud.lamia96@gmail.com>
2023-05-18 23:54:37 +0200

    Test Naming Convention

---
+++ src/scoringSystem/helpers/__tests__/checkNamingConventions.test.js
@@ -0,0 +1,47 @@
+const acorn = require("acorn");
+const walk = require("acorn-walk");
+const checkNamingConventions = require("../checkNamingConventions");
+
+test('should detect incorrect naming conventions', () => {
+    const code = `
+        let myVariable = 1;
+        let my_variable = 2;
+        let MyVariable = 3;
+    `;
+    const AST = acorn.Parser.parse(code);
+
+    expect(checkNamingConventions(AST, ['camelCase'])).toBe(2);
+});
+
+test('should not report correct naming conventions', () => {
+    const code = `
+        let myVariable = 1;
+        let anotherVariable = 2;
+        let thirdVariable = 3;
+    `;
+    const AST = acorn.Parser.parse(code);
+
+    expect(checkNamingConventions(AST, ['camelCase'])).toBe(0);
+});
+
+test('should handle multiple naming conventions', () => {
+    const code = `
+        let myVariable = 1;
+        let another_variable = 2;
+        let ThirdVariable = 3;
+    `;
+    const AST = acorn.Parser.parse(code);
+
+    expect(checkNamingConventions(AST, ['camelCase', 'snake_case', 'PascalCase'])).toBe(0);
+});
+
+test('should handle no naming conventions', () => {
+    const code = `
+        let myVariable = 1;
+        let another_variable = 2;
+        let ThirdVariable = 3;
+    `;
+    const AST = acorn.Parser.parse(code);
+
+    expect(checkNamingConventions(AST, [])).toBe(3);
+});
