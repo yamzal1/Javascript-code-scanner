5ce88d9463db81641262a3cb06a12edd011b8486
“LAMIASASAOUD” <sasaoud.lamia96@gmail.com>
2023-05-18 12:37:16 +0200

    Update focntion checkVariableEcplicit

--- src/scoringSystem/rules/checkVariablesExplicit.js
+++ src/scoringSystem/rules/checkVariablesExplicit.js
@@ -1,3 +1,9 @@
+function isImplicitVariable(variableName) {
+  // Vérifier si le nom de la variable est implicite (un seul mot et contient des lettres consécutives répétées)
+  const regex = /^(\w)\1+$/;
+  return regex.test(variableName);
+}
+
 module.exports = function checkVariablesExplicit(AST) {
   let implicitVariablesCount = 0;
 
@@ -11,7 +17,7 @@ module.exports = function checkVariablesExplicit(AST) {
       ) {
         const variableName = node.declarations[0].id.name;
 
-        // Vérifier si le nom de la variable est implicite
+        // Vérifier si le nom de la variable est implicite (un seul mot et contient des lettres consécutives répétées)
         if (isImplicitVariable(variableName)) {
           implicitVariablesCount++;
         }
@@ -21,23 +27,3 @@ module.exports = function checkVariablesExplicit(AST) {
 
   console.log('Nombre de variables implicites :', implicitVariablesCount);
 };
-
-function isImplicitVariable(variableName) {
-  // Vérifier si le nom de la variable est implicite (contient la même lettre plus de deux fois)
-  const charCount = new Map();
-  for (const char of variableName) {
-    if (charCount.has(char)) {
-      charCount.set(char, charCount.get(char) + 1);
-    } else {
-      charCount.set(char, 1);
-    }
-  }
-
-  for (const count of charCount.values()) {
-    if (count > 2) {
-      return true;
-    }
-  }
-
-  return false;
-}
