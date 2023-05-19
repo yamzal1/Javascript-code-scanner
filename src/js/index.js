532e92c1e23a829c67acbb142532f3cd193936b8
Wissal <wissal.khila@etu.u-paris.fr>
2023-05-19 13:35:55 +0200

    fixed merge conflict
    Co-authored-by: lnguyentuong <lnguyentuong@users.noreply.github.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,9 @@
 const {Parser} = require('acorn')
 const walk = require('acorn-walk')
 
-
 module.exports = function generateSyntaxTreeJS(input){
     let comments = []
-    let tree = Parser.parse(input, {ecmaVersion: "latest", onComment: (b, t)=> comments.push(t)})
+    let tree = Parser.parse(input, {ecmaVersion: "latest", onComment: (b, t)=> comments.push(t), locations: true})
     return {
         parser: Parser,
         comments: comments,
--- src/scoringSystem/helpers/checkIndent.js
+++ src/scoringSystem/helpers/checkIndent.js
@@ -1,3 +1,28 @@
-module.exports = function checkIndent(tree, walk){
-    return 0
+module.exports = function checkIndent(input,tokens){
+    errCount = 0
+    indentErrorLines = new Set()
+    indentLevel = 0
+    lines = input.split('\n')
+    currentLine = 1
+    for(let token of tokens){
+        tokenLine = token.loc.start.line
+        if(token.type.label === "{") {
+            indentLevel+=4
+            currentLine = tokenLine
+        }
+        if(token.type.label === '}'){
+            indentLevel-=4
+        }
+        if(tokenLine===currentLine) continue
+        let whiteSpaces = lines[tokenLine-1].match(/^\s*/)
+        let leadingSpaces
+        if(!whiteSpaces) leadingSpaces = 0
+        else leadingSpaces = whiteSpaces[0].length
+        
+        if(leadingSpaces !== indentLevel){
+            indentErrorLines.add(tokenLine)
+            errCount++
+        }
+    }
+    return indentErrorLines
 }
